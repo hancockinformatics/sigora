1dfb796c21da32cdcc97836c60f64756a700e764
Travis <travis.m.blimkie@gmail.com>
2021-06-18 15:57:05 -0700

    Tidied formatting of getURL.R

--- R/getURL.R
+++ R/getURL.R
@@ -1,17 +1,34 @@
-getURL<-function(yy,i){
-url<-''
-repo<-ifelse(substr(x=yy$summary_results[i,1],start=1,2)=='R-','rea','keg')
+getURL <- function(yy, i) {
+  url <- ""
 
-if(repo=='rea'){
-cat(paste("assuming this is a result from Reactome ...","\n"))
-url<-paste("https://reactome.org/ContentService/exporter/diagram//",
-yy$summary_results[i,1],".png?flg=",paste(getGenes(yy,i)[,4],sep='',
-collapse=','),sep='')}
+  repo <- ifelse(
+    substr(x = yy$summary_results[i, 1], start = 1, 2) == "R-",
+    "rea",
+    "keg"
+  )
 
-if(repo=='keg'){
-cat(paste("assuming this is a result from KEGG ...","\n"))
-url<-paste("https://www.genome.jp/kegg-bin/show_pathway?",
-tolower(yy$summary_results[i,1]),
-"+",paste(getGenes(yy,i)[,4],sep='',collapse='+'),sep='')}
-url
+  if (repo == "rea") {
+    cat("Assuming this is a result from Reactome...\n")
+
+    url <- paste(
+      "https://reactome.org/ContentService/exporter/diagram//",
+      yy$summary_results[i, 1],
+      ".png?flg=",
+      paste(getGenes(yy, i)[, 4], sep = "", collapse = ","),
+      sep = ""
+    )
+  }
+
+  if (repo == "keg") {
+    cat("Assuming this is a result from KEGG...\n")
+
+    url <- paste(
+      "https://www.genome.jp/kegg-bin/show_pathway?",
+      tolower(yy$summary_results[i, 1]),
+      "+",
+      paste(getGenes(yy, i)[, 4], sep = "", collapse = "+"),
+      sep = ""
+    )
+  }
+  url
 }
